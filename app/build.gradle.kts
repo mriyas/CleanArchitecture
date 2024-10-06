911bb886cdc3e9904fceb431dfcca48e9fb5d529
Riyas <riyaskokkur@gmail.com>
2024-10-06 20:01:56 +0100

    Upgraded to material 3 apis

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,6 @@ dependencies {
 
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.compose.material)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
